524c572d43450d57fdba2d5f975867560bdd8737
Maurizio <proiettomaurizio97@gmail.com>
2021-11-02 16:48:58 +0100

    ciclo for

--- js/script.js
+++ js/script.js
@@ -1,25 +1,62 @@
 // DONE=> 1. l'utente sceglie il livello di difficoltà
-// 2. faccio comparire il gioco
-// 3 se l'utente clicca su una delle caselle del gioco la casella cambia colore
+// DONE=> 2 in base alla scelta creo una variabile che mi salva il livello
+    // 2.1 se scegli 1 compare il gioco con una griglia 10 x 10
+    // 2.2 se scegli 2 compare il gioco con una griglia 9 x 9
+    // 2.3 se scegli 3 compare il gioco con una griglia 7 x 7
+// DONE=> 3 creo una funzione per creare un elemento
+// 4 creo un ciclo che mi permetta di inserire i quadrati nel DOM
+// 5 se l'utente clicca su una delle caselle del gioco la casella cambia colore
 
 
+
+// creo un promt per la scelta del livello
 const choiseLiv = parseInt(prompt("scegli il livello digitando 1, 2 o 3"));
 
-const gameCont = document.querySelector(".container-game");
-console.log(gameCont);
+const squareCont = document.querySelector(".container-game");
+console.log(squareCont);
 
+// 3. variabile con l'elemento creato
+let gameSquare = genElement("div", "square");
+let spanSquare = document.createElement("span")
+console.log(spanSquare);
+//variabile contenente il livello scelto
+let liv;
+if(choiseLiv == 1){
+    i = 100;
+} else if(choiseLiv == 2){
+    i = 81;
+} else if (choiseLiv == 3){
+    i = 49;
+}
+console.log(i);
 
+// 4. ciclo per inserire i quadrati nel dom
+for ( let i = 0; i < liv ; i++){
 
+    spanSquare.append(i);
+    gameSquare.append(spanSquare);
 
+    if( i == 100){
+        gameSquare.classList.add("liv-1");
+    } else if (i == 81){
+        gameSquare.classList.add("liv-2");
+    } else if (i == 49){
+        gameSquare.classList.add("liv-3");
+    }
 
+    squareCont.append(gameSquare);
+    console.log(squareCont);
+}
 
-
-
+console.log(squareCont);
 
 
 
 /* funzioni utili */
-
-function genElement(){
-    let el = document.createElement("div");
+//funzione per generare un elemento
+function genElement(typeEl, classEl ){
+    let elem = document.createElement(typeEl);
+    elem.classList.add(classEl)
+    return elem
 }
+
