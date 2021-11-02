bf59d67ecd1f41870033d411daad751116d026f0
Maurizio <proiettomaurizio97@gmail.com>
2021-11-02 16:02:00 +0100

    i add the comments

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
-console.log("ciao");
+// DONE=> 1. l'utente sceglie il livello di difficoltà
+// 2. faccio comparire il gioco
+// 3 se l'utente clicca su una delle caselle del gioco la casella cambia colore
+
+
+const choiseLiv = parseInt(prompt("scegli il livello digitando 1, 2 o 3"));
+
+const gameCont = document.querySelector(".container-game");
+console.log(gameCont);
+
+
+
+
+
+
+
+
+
+
+
+/* funzioni utili */
+
+function genElement(){
+    let el = document.createElement("div");
+}
