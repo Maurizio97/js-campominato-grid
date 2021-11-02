e66a2f264ca93f0b2f4f4c8e85c236a0699b731f
Maurizio <proiettomaurizio97@gmail.com>
2021-11-02 17:22:49 +0100

    js complete

--- js/script.js
+++ js/script.js
@@ -4,8 +4,8 @@
     // 2.2 se scegli 2 compare il gioco con una griglia 9 x 9
     // 2.3 se scegli 3 compare il gioco con una griglia 7 x 7
 // DONE=> 3 creo una funzione per creare un elemento
-// 4 creo un ciclo che mi permetta di inserire i quadrati nel DOM
-// 5 se l'utente clicca su una delle caselle del gioco la casella cambia colore
+// DONE=> 4 creo un ciclo che mi permetta di inserire i quadrati nel DOM
+    // 4.1 se l'utente clicca su una delle caselle del gioco la casella cambia colore
 
 
 
@@ -15,10 +15,6 @@ const choiseLiv = parseInt(prompt("scegli il livello digitando 1, 2 o 3"));
 const squareCont = document.querySelector(".container-game");
 console.log(squareCont);
 
-// 3. variabile con l'elemento creato
-let gameSquare = genElement("div", "square");
-let spanSquare = document.createElement("span")
-console.log(spanSquare);
 //variabile contenente il livello scelto
 let liv;
 if(choiseLiv == 1){
@@ -32,23 +28,32 @@ if(choiseLiv == 1){
 
 // 4. ciclo per inserire i quadrati nel dom
 for ( let i = 1; i <= liv ; i++){
-
+    // 3. variabile con l'elemento creato
+    let gameSquare = genElement("div", "square");
+    let spanSquare = document.createElement("span");
     spanSquare.append(i);
-    gameSquare.append(spanSquare);
+    gameSquare.appendChild(spanSquare);
 
-    if( i == 100){
+    if( liv == 100){
         gameSquare.classList.add("liv-1");
-    } else if (i == 81){
+    } else if (liv == 81){
         gameSquare.classList.add("liv-2");
-    } else if (i == 49){
+    } else if (liv == 49){
         gameSquare.classList.add("liv-3");
     }
+    
+    
+    gameSquare.addEventListener("click",
 
+        function(){
+            gameSquare.classList.add("active");
+        }
+
+    )
     squareCont.append(gameSquare);
-    console.log(squareCont);
 }
 
-console.log(squareCont);
+
 
 
 
